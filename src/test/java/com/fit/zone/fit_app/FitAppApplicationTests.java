46314743f59b2207f8744b071b778479095bc504
yan pizarro <pizarroestevez2@gmail.como>
2025-08-22 14:21:40 -0300

    Creating the project

---
+++ src/test/java/com/fit/zone/fit_app/FitAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fit.zone.fit_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
